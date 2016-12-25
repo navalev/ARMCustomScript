321d10057214d6921ff77b2ee7b309ba892372e6
Nava Vaisman Levy <navale@microsoft.com>
2016-12-25 17:55:28 +0200

    template and script

---
+++ downloadInstaller.ps1
@@ -0,0 +1,11 @@
+﻿$url = "https://go.microsoft.com/fwlink/?LinkId=254279&clcid=0x409"
+$start_time = Get-Date
+$output = "windowsazurexplatcli.exe"
+
+# download
+Invoke-WebRequest -Uri $url -OutFile $output
+
+Write-Output "Download Time: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+
+# install
+& ./$output
