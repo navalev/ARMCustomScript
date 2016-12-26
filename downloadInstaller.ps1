2abe06a205545f7c97bc216b71f0a9e8da4e068b
Nava Vaisman Levy <navalev@users.noreply.github.com>
2016-12-26 12:39:13 +0200

    Update downloadInstaller.ps1

--- downloadInstaller.ps1
+++ downloadInstaller.ps1
@@ -1,11 +1,9 @@
 ﻿$url = "https://zertotestdownload.blob.core.windows.net/test/windowsazurexplatcli.exe"
 $start_time = Get-Date
-$output = "windowsazurexplatcli.exe"
+$output = "$PSScriptRoot\windowsazurexplatcli.exe"
 
 # download
 Invoke-WebRequest -Uri $url -OutFile $output
-
-Write-Output "Download Time: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
 # install
 & ./$output
